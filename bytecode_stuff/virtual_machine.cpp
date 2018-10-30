4b1bcdb21404f47895187b62d26a78c8d5314ea9
TetroVolt <raymond@sutrisno.me>
2018-10-29 22:18:22 -0500

    vm branch if

--- bytecode_stuff/virtual_machine.cpp
+++ bytecode_stuff/virtual_machine.cpp
@@ -16,10 +16,14 @@ int run(uint64_t *main_program, size_t length) {
   uint8_t bcode;
 
   int64_t a, b; // temp registers a and b
-  uint64_t ref; // temp registers a and b
+  int64_t addr_jump; // for address jumping
+  uint64_t oref; // reference argument
+  char * str; // for printing strings
   Operand temp(a); // temp operand
 
-  #define VAL() operand_stack.top().value;\
+  #define VAL() (operand_stack.top().o_type) ?\
+                operand_stack.top().value :\
+                function_stack_variables[operand_stack.top().reference];\
                 operand_stack.pop();\
                 operand_stack_scope.top()--
 
@@ -28,22 +32,51 @@ int run(uint64_t *main_program, size_t length) {
                 operand_stack_scope.top()--
 
   #define VAB() b = VAL(); a = VAL()
-  #define ARB() b = VAL(); ref = REF()
+  #define ARB() b = VAL(); oref = REF()
 
   #define ABO(O) VAB(); temp = Operand((int64_t)(a O b));\
                         operand_stack.push(temp);\
                         operand_stack_scope.top()++
 
+  #define INS_ADDR_MASK 0x00ffffffffffffff
   while (pc < length) {
     ins = main_program[pc++];
-    bcode = ins >> 56;
+    bcode = ins & 0xff;
+    addr_jump = ins;
+    addr_jump >>= 8;
+    oref = ins >> 8;
 
     switch (bcode) {
       case ByteCode_ISET::nop:
         break;
       case ByteCode_ISET::exit_prog:
-        return (ins & INS_ADDR_MASK);
+        return addr_jump;
 
+      case ByteCode_ISET::f_return_item:
+        assert((a = operand_stack_scope.top()) > 0);
+        temp = operand_stack.top();
+        for (; a > 0; a--) operand_stack.pop(); operand_stack_scope.pop();
+        operand_stack.push(temp); operand_stack_scope.top()++;
+        pc = function_stack.top(); function_stack.pop();
+        function_stack_variables.pop();
+        break;
+
+      case ByteCode_ISET::f_push: // call a function
+        function_stack.push(pc); // save current pc on stack for later
+        pc = oref >> 8;   // first 6 bytes to program counter
+        a  = oref & 0xff; // 7th byte = number of stack frame variables
+        function_stack_variables.push(vector<int64_t>(a,0));
+          // push empty vector for stack frame variables
+        operand_stack_scope.push(0); // push a new operand stack scope
+        break;
+
+      case ByteCode_ISET::branch_if:
+        a = VAL(); if (a) pc += addr_jump; break;
+      case ByteCode_ISET::branch_nif:
+        a = VAL(); if (!a) pc += addr_jump; break;
+      case ByteCode_ISET::branch:
+        pc += addr_jump; break;
+ 
       case ByteCode_ISET::addi:
         assert(operand_stack_scope.top() >= 2);
         ABO(+); break;
@@ -85,8 +118,17 @@ int run(uint64_t *main_program, size_t length) {
         cout << a;
         break;
 
-      case ByteCode_ISET::print_sc: // output string constant
-        cout << (((char*)(main_program)) + (ins & INS_ADDR_MASK)); break;
+      case ByteCode_ISET::print_si: // print string from instruction addr
+        str = ((char*)(main_program)) + ((char)(oref));
+        cout << str << endl;
+        break;
+
+      case ByteCode_ISET::print_so: // print string from operand stack
+        oref = REF();
+        str = ((char*)(main_program)) + ((char)(oref));
+        cout << str << endl;
+        break;
+
       case ByteCode_ISET::print_sp:
         cout << " "; break;
 
@@ -97,7 +139,7 @@ int run(uint64_t *main_program, size_t length) {
           operand_stack.top() = Operand(a);
         }
         ARB();
-        function_stack_variables.top()[ref] = b;
+        function_stack_variables.top()[oref] = b;
         break;
 
       case ByteCode_ISET::deref:  // made useless by assn instruction dereference check
@@ -108,16 +150,14 @@ int run(uint64_t *main_program, size_t length) {
         break;
 
       case ByteCode_ISET::o_push_const_ri:
-        ref = (ins & INS_ADDR_MASK) >> 3;
-        a = (int64_t)(main_program[ref]);
+        a = (int64_t)(main_program[oref]);
         temp = Operand(a);
         operand_stack.push(temp);
         operand_stack_scope.top()++;
         break;
 
       case ByteCode_ISET::o_push_const_rr:
-        ref = (ins & INS_ADDR_MASK);
-        temp = Operand(ref);
+        temp = Operand(oref);
         operand_stack.push(temp);
         operand_stack_scope.top()++;
         break;
@@ -141,28 +181,7 @@ int run(uint64_t *main_program, size_t length) {
         operand_stack.pop();
         break;
 
-      case ByteCode_ISET::f_return_item:
-        assert((a = operand_stack_scope.top()) > 0);
-        temp = operand_stack.top();
-        for (; a > 0; a--) operand_stack.pop(); operand_stack_scope.pop();
-        operand_stack.push(temp); operand_stack_scope.top()++;
-        pc = function_stack.top(); function_stack.pop();
-        function_stack_variables.pop();
-        break;
-
-      case ByteCode_ISET::f_push:
-        function_stack.push(pc);
-        pc = (ins & (INS_ADDR_MASK >> 8)) >> 3;
-        a  = (ins & INS_ADDR_MASK) >> 48;
-        function_stack_variables.push(vector<int64_t>(a,0));
-        operand_stack_scope.push(0);
-        break;
-
-      case ByteCode_ISET::branch_if:
-        a = VAL(); if (!a) break;
-      case ByteCode_ISET::branch:
-        pc = (ins & INS_ADDR_MASK) >> 3; break;
-    }
+   }
   }
 
   return 0;
